dd7d9331ec37486c7b2c3482384c5d5fe07eea24
Darshan K ⑦ <kbdarshan1008@gmail.com>
2024-09-04 16:04:41 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,30 @@
+// JavaScript for Typing Effect
+document.addEventListener("DOMContentLoaded", function () {
+    const typingText = document.querySelector(".typing-text");
+    const words = ["Frontend Developer", "Web Designer", "Tech Enthusiast"];
+    let wordIndex = 0;
+    let letterIndex = 0;
+
+    function type() {
+        if (letterIndex < words[wordIndex].length) {
+            typingText.textContent += words[wordIndex].charAt(letterIndex);
+            letterIndex++;
+            setTimeout(type, 100);
+        } else {
+            setTimeout(erase, 1000);
+        }
+    }
+
+    function erase() {
+        if (letterIndex > 0) {
+            typingText.textContent = words[wordIndex].substring(0, letterIndex - 1);
+            letterIndex--;
+            setTimeout(erase, 50);
+        } else {
+            wordIndex = (wordIndex + 1) % words.length;
+            setTimeout(type, 500);
+        }
+    }
+
+    type();
+});
